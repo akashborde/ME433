446f79ccd8ef1ea271b93a90c44b5a5f7df7f35f
akashborde <akashborde2019@u.northwestern.edu>
2019-04-16 18:01:44 -0500

    some spi starter code

--- HW4/main.c
+++ HW4/main.c
@@ -9,55 +9,34 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
 #include<xc.h>           // processor SFR definitions
 #include<sys/attribs.h>  // __ISR macro
-
 #include "pragmas.h"
+#include "spi.h"
 
-//SYSCLK = 48MHz, Core Timer = 24MHz, so one count every 0.0000416 millisec
-//Desired period = 0.5ms
-//Count to 12019 to get to 0.5ms
-
-#define HALF_MILLISEC_IN_COUNTS 12019
 
 int main() {
 
     __builtin_disable_interrupts();
-
     // set the CP0 CONFIG register to indicate that kseg0 is cacheable (0x3)
     __builtin_mtc0(_CP0_CONFIG, _CP0_CONFIG_SELECT, 0xa4210583);
-
     // 0 data RAM access wait states
     BMXCONbits.BMXWSDRM = 0x0;
-
     // enable multi vector interrupts
     INTCONbits.MVEC = 0x1;
-
     // disable JTAG to get pins back
     DDPCONbits.JTAGEN = 0;
 
-    // do your TRIS and LAT commands here
-    
-    /*
-     * Add initializations in main() to make the pushbutton pin an input pin,
-     * and the LED pin as an output that is initially high. 
-     * Upload the code and verify that the LED turns on. 
-     */
-    
     // Pushbutton pin: B4
     TRISBbits.TRISB4 = 1; // Set it as an input
-        
-    // Green LED pin:  A4
+   // Green LED pin:  A4
     TRISAbits.TRISA4 = 0; //set as output
     LATAbits.LATA4 = 1;   //initialized as ON
     
     __builtin_enable_interrupts();
     
-    
     while(1) 
     {
-        
         //if button pushed, turn LED off.
         if (PORTBbits.RB4 == 0) //read this input pin
         {
@@ -67,6 +46,5 @@ int main() {
         {        
             LATAbits.LATA4 = 1; // turn it on
         }     
-    
     }
 }
---
+++ HW4/spi.h
@@ -0,0 +1,27 @@
+/* 
+ * File:   spi.h
+ * Author: akashborde
+ *
+ * Created on April 16, 2019, 4:28 PM
+ */
+
+#ifndef SPI_H
+#define	SPI_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+    
+    void initSPI1(); // initalizes SPI1 channel
+    char SPI1_IO(char write); //generic communication
+    void setVoltage(char channel, int voltage);
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* SPI_H */
+
