21c28f6315e14d193039ebcf2ec55c3a400949c0
akashborde <akashborde2019@u.northwestern.edu>
2019-04-26 11:41:25 -0500

    Frame rate counter added

--- HW6/HW6.X/main.c
+++ HW6/HW6.X/main.c
@@ -13,6 +13,10 @@
 #include <sys/attribs.h> // __ISR macro (not sure)
 #include "ili9341.h" //LCD 
 
+#define COUNTS_PER_SEC 24000000
+//one count every 0.0000000416 sec
+//24000000 counts / sec
+
 
 int main() 
 {
@@ -26,19 +30,26 @@ int main()
     LCD_clearScreen(ILI9341_PINK); 
      
     _CP0_SET_COUNT(0);
-    int i;
+    int i = 0;
+    int c1, c2;
+    double frames_sec;
     char m[100];
-    
+       
     while(1)
     {    
         
         if((_CP0_GET_COUNT() > TEN_HZ_PERIOD) && (i < 101)) //check if the pic hangs
         {    
-              
+            c1 = _CP0_GET_COUNT();
             sprintf(m, "Hello, world %d", i); //put the string i = (number) in a char array
             LCD_print(m, START_X, START_Y, ILI9341_BLUE, ILI9341_PINK);
-            LCD_progBar(START_X, START_Y + 10, PROG_BAR_LEN, PROG_BAR_HEIGHT, i, ILI9341_CYAN,ILI9341_BLACK); // draw a 30% full bar
+            LCD_progBar(START_X, START_Y + 10, PROG_BAR_LEN, PROG_BAR_HEIGHT, i, ILI9341_CYAN,ILI9341_BLACK); // draw a 30% full bar            
+            c2 = _CP0_GET_COUNT();
+            
+            frames_sec = (double) COUNTS_PER_SEC / (c2-c1);
+            sprintf(m, "Frame Rate = %.2f frames/sec", frames_sec); 
 
+            LCD_print(m, START_X, START_Y + 20, ILI9341_BLUE, ILI9341_PINK);
             LED_Invert_A4();
            _CP0_SET_COUNT(0);
            i++;
@@ -46,4 +57,3 @@ int main()
     }
     
 }
-
